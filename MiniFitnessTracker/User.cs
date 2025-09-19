48332385508a851397fce74b0743989a78b18d2b
aml <aml.mstfa0447111@compit.aun.edu.eg>
2025-09-20 00:11:07 +0300

    Restore Class2.cs and Class3.cs

--- MiniFitnessTracker/User.cs
+++ MiniFitnessTracker/User.cs
@@ -10,48 +10,18 @@ namespace MiniFitnessTracker
     {
         public string Name { get; set; }
         public int Age { get; set; }
-        public double Height { get; set; }
-        public double Weight { get; set; }
-        public List<WorkoutPlan> workoutPlans { get; set; } = new List<WorkoutPlan>();
-        public bool Updateprofile(string name, int age, double height, double weight)
-        {
-            bool valid = true;
-            if (string.IsNullOrWhiteSpace(name))
-            {
-                Console.WriteLine("Name cannot be empty");
-                valid = false;
-            }
-            if (age < 0)
-            {
-                Console.WriteLine("Age must be positive");
-                valid = false;
+        public double Weight { get; set; } // in kg
+        public double Height { get; set; } // in cm
+        public List<WorkoutPlan> WorkoutPlans { get; set; }
 
-            }
-            if (height <= 0)
-            {
-                Console.WriteLine("Height must be Positive");
-                valid = false;
-            }
-            if (weight <= 0)
-            {
-                Console.WriteLine("Weight must be Positive");
-                valid = false;
-            }
-            if (!valid)
-            {
-                return false;
-            }
+        public User(string name, int age, double weight, double height)
+        {
             Name = name;
             Age = age;
             Weight = weight;
             Height = height;
-            return true;
-
+            WorkoutPlans = new List<WorkoutPlan>();
         }
-        public string viewprofile()
-        {
-            return $"Name : {Name}\n Age : {Age}\n Weight : {Weight}KG\n Height : {Height}cm ";
-        }
+        
     }
 }
-
