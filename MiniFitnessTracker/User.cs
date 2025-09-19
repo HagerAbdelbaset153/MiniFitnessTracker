937a0069297e75805146f991b7e315fe628cefc4
HagerAbdelbaset153 <hager1532006@gmail.com>
2025-09-19 21:33:27 +0300

    Rename some classes

--- MiniFitnessTracker/User.cs
+++ MiniFitnessTracker/User.cs
@@ -10,18 +10,48 @@ namespace MiniFitnessTracker
     {
         public string Name { get; set; }
         public int Age { get; set; }
-        public double Weight { get; set; } // in kg
-        public double Height { get; set; } // in cm
-        public List<WorkoutPlan> WorkoutPlans { get; set; }
-
-        public User(string name, int age, double weight, double height)
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public List<WorkoutPlan> workoutPlans { get; set; } = new List<WorkoutPlan>();
+        public bool Updateprofile(string name, int age, double height, double weight)
         {
+            bool valid = true;
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("Name cannot be empty");
+                valid = false;
+            }
+            if (age < 0)
+            {
+                Console.WriteLine("Age must be positive");
+                valid = false;
+
+            }
+            if (height <= 0)
+            {
+                Console.WriteLine("Height must be Positive");
+                valid = false;
+            }
+            if (weight <= 0)
+            {
+                Console.WriteLine("Weight must be Positive");
+                valid = false;
+            }
+            if (!valid)
+            {
+                return false;
+            }
             Name = name;
             Age = age;
             Weight = weight;
             Height = height;
-            WorkoutPlans = new List<WorkoutPlan>();
+            return true;
+
         }
-        
+        public string viewprofile()
+        {
+            return $"Name : {Name}\n Age : {Age}\n Weight : {Weight}KG\n Height : {Height}cm ";
+        }
     }
 }
+
