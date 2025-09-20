356bc53f59e21604a1339b8893419a69932a38a9
HagerAbdelbaset153 <hager1532006@gmail.com>
2025-09-20 23:45:07 +0300

    Rename some classes

--- MiniFitnessTracker/User.cs
+++ MiniFitnessTracker/User.cs
@@ -5,115 +5,57 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-
 namespace MiniFitnessTracker
 {
-    public class User
+    public class user
     {
         public string Name { get; set; }
         public int Age { get; set; }
-        public double Weight { get; set; } // in kg
-        public double Height { get; set; } // in cm
-        public List<WorkoutPlan> WorkoutPlans { get; set; } = new List<WorkoutPlan>();
-
-        public User(string name, int age, double weight, double height)
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public List<WorkoutPlan> workoutPlans { get; set; } = new List<WorkoutPlan>();
+        public bool Updateprofile(string name, int age, double height, double weight)
         {
-            Name = name;
-            Age = age;
-            Weight = weight;
-            Height = height;
-        }
-
-
-        public string CalculateBMI()
-        {
-            double heightInMeters = Height / 100; // لأن الطول متخزن بالـ cm
-            double bmi = Weight / (heightInMeters * heightInMeters);
-
-            string category;
-            if (bmi < 18.5) category = "Underweight";
-            else if (bmi < 25) category = "Normal";
-            else if (bmi < 30) category = "Overweight";
-            else category = "Obese";
-
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            return $"Your BMI is {bmi:F2} ({category})";
-            
-        }
-
-
-
-        public double? WeightGoal { get; set; } // null لو مفيش هدف متحدد
-
-        public void SetGoal(double targetWeight)
-        {
-            if (targetWeight <= 0)
+            Console.ForegroundColor = ConsoleColor.Red;
+            bool valid = true;
+            if (string.IsNullOrWhiteSpace(name))
             {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.WriteLine("Invalid goal weight.");
-                Console.ResetColor();
-                return;
+               
+                Console.WriteLine("Name cannot be empty");
+              
+                valid = false;
             }
-            WeightGoal = targetWeight;
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"Goal set: {WeightGoal} kg");
-            Console.ResetColor();
-        }
-
-        public string CheckGoalProgress()
-        {
-            if (WeightGoal == null)
+            if (age < 0)
             {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                return "No goal set yet.";
-            }
+                Console.WriteLine("Age must be positive");
+                valid = false;
 
-            double difference = Weight - WeightGoal.Value;
-            if (difference == 0)
+            }
+            if (height <= 0)
             {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                return "Congratulations  You achieved your goal !";
+                Console.WriteLine("Height must be Positive");
+                valid = false;
             }
-            else if (difference > 0)
+            if (weight <= 0)
             {
-                Console.ForegroundColor= ConsoleColor.Yellow;
-                return $"You need to lose {difference:F1} kg to reach your goal.";
+                Console.WriteLine("Weight must be Positive");
+                valid = false;
             }
-            else
+            if (!valid)
             {
-                Console.ForegroundColor=ConsoleColor.Yellow;
-                return $"Great! You are {Math.Abs(difference):F1} kg under your goal.";
-            }
-        }
-
-
-
-
-
-        public bool UpdateProfile(string name, int age, double height, double weight)
-        {
-            if (string.IsNullOrWhiteSpace(name) || age <= 0 || height <= 0 || weight <= 0)
                 return false;
-
+            }
             Name = name;
             Age = age;
-            Height = height;
             Weight = weight;
+            Height = height;
             return true;
-        }
 
-        public string ViewProfile()
+        }
+        public string viewprofile()
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            return $"Name: {Name}\nAge: {Age}\nWeight: {Weight} kg\nHeight: {Height} cm";
-            
+            return $"Name : {Name}\n Age : {Age}\n Weight : {Weight}KG\n Height : {Height}cm ";
         }
     }
 }
-
-
-
-
-
-
 
